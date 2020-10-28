165161b3e2cd73ee94650d585667175a2366a154
leshchenko1979 <60138889+leshchenko1979@users.noreply.github.com>
2020-10-28 07:28:09 +0300

    исключение, если дубли не пускать мусор в основную базу

--- s4_final_preparation.py
+++ s4_final_preparation.py
@@ -4,11 +4,23 @@ import pandas as pd
 import shutil
 import datetime
 
+
+def sanity_check():
+    df = pd.read_csv(config['files']['final_offers'])
+    
+    duplicates = df.shape[0] - df.drop_duplicates.shape[0]
+    
+    if duplicates:
+        raise ValueError(f'{duplicates} duplicates in the final file, aborting')
+
+
 with timer('Downloading ads'):
     import download_ads
 with timer('Updating regions file'):
     import update_regions
 run_prep('final', what_to_gauge=config['files']['final_offers'])
+
+sanity_check()
 
 log('Backing up the archive') 
 shutil.copy(f"{config['dirs']['data']}\\{config['files']['archive_file']}", f"{config['dirs']['archive']}\\{datetime.date.today()}.csv")
