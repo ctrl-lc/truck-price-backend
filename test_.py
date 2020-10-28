0ccc9ea0af2c566ad191a82f5609fc61981609c5
leshchenko1979 <60138889+leshchenko1979@users.noreply.github.com>
2020-10-28 09:16:50 +0300

    Нет обработки ошибок при загрузке регионов Fixes #4

--- test_.py
+++ test_.py
@@ -1,5 +1,30 @@
+from update_regions import fetch_slice
+from aiohttp.client import ClientSession
 from s4_final_preparation import sanity_check
 import pytest
 
 def test_sanity_check():
     sanity_check()
+
+
+@pytest.mark.asyncio    
+async def test_update_regions_403_response(monkeypatch):
+    
+    class MockRequest():
+        async def __aenter__(*args):
+            return MockResponse()
+        async def __await__(*args):
+            pass
+        async def __aexit__(*args):
+            pass
+
+        
+    class MockResponse():
+        status = 403
+        
+        
+    session = ClientSession()
+    monkeypatch.setattr(session, 'request', lambda *args, **kwargs: MockRequest())
+    
+    assert await fetch_slice('Москва', session) is None
+    
