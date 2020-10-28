6ba0cd73f7837157bdac2e377402b1d1fc498838
leshchenko1979 <60138889+leshchenko1979@users.noreply.github.com>
2020-10-28 07:49:39 +0300

    тест работает

--- s4_final_preparation.py
+++ s4_final_preparation.py
@@ -6,22 +6,29 @@ import datetime
 
 
 def sanity_check():
-    df = pd.read_csv(config['files']['final_offers'])
+    df = pd.read_csv(
+        f"{config['dirs']['data']}/{config['files']['final_offers']}",
+        encoding = 'utf-8', delimiter = ';')
     
-    duplicates = df.shape[0] - df.drop_duplicates.shape[0]
+    duplicates = df.shape[0] - df.drop_duplicates().shape[0]
     
     if duplicates:
         raise ValueError(f'{duplicates} duplicates in the final file, aborting')
 
 
-with timer('Downloading ads'):
-    import download_ads
-with timer('Updating regions file'):
-    import update_regions
-run_prep('final', what_to_gauge=config['files']['final_offers'])
+def main():
+    with timer('Downloading ads'):
+        import download_ads
+    with timer('Updating regions file'):
+        import update_regions
+    run_prep('final', what_to_gauge=config['files']['final_offers'])
 
-sanity_check()
+    sanity_check()
 
-log('Backing up the archive') 
-shutil.copy(f"{config['dirs']['data']}\\{config['files']['archive_file']}", f"{config['dirs']['archive']}\\{datetime.date.today()}.csv")
-gauge(config['files']['archive_file'])
+    log('Backing up the archive') 
+    shutil.copy(f"{config['dirs']['data']}\\{config['files']['archive_file']}", f"{config['dirs']['archive']}\\{datetime.date.today()}.csv")
+    gauge(config['files']['archive_file'])
+    
+
+if __name__ == "__main__":
+    main()
--- test_.py
+++ test_.py
@@ -1,4 +1,5 @@
 from s4_final_preparation import sanity_check
+import pytest
 
 def test_sanity_check():
     sanity_check()
